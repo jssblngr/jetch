c5b288f587224a9397973e6ea9ea504c0baa8432
jssblngr <jessebelanger@gmail.com>
2021-01-17 17:38:52 -0500

    Added color options for greyscale and rainbow.  Greyscale bug affects all other colors once used once.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let btn = document.getElementById("countbutton")
+let adjustbtn = document.getElementById("countbutton")
 let clearbtn = document.getElementById("clearbutton")
-btn.addEventListener("click", adjustGrid)
+let blackbtn = document.getElementById("blackbtn")
+let greybtn = document.getElementById("greybtn")
+let rainbowbtn = document.getElementById("rainbowbtn")
+
+adjustbtn.addEventListener("click", adjustGrid)
 clearbtn.addEventListener("click", clearGrid)
+blackbtn.addEventListener("click", blackBtn)
+greybtn.addEventListener("click", greyBtn)
+rainbowbtn.addEventListener("click", rainbowBtn)
+
+let globalColor = ""
 
 function defaultGrid() {
-    createGrid(50);
     document.getElementById("countsize").value = 50;
+    createGrid(50);
 }
 
 function createGrid(rows) {
@@ -16,14 +25,15 @@ function createGrid(rows) {
         var etchCell = document.createElement("div");
         etchCell.classList.add("etchcell")
         etchContainer.appendChild(etchCell);
-        etchCell.addEventListener("mouseenter", function () {
-            this.style.backgroundColor = "black";
+        etchCell.addEventListener("mouseenter", function setColor() {
+                this.style.backgroundColor = `${globalColor}`;
         })
     }
 }
 
 function adjustGrid() {
     let rows = document.getElementById("countsize").value;
+    rows = document.getElementById("countsize").value;
     clearGrid();
     createGrid(rows);
 }
@@ -33,6 +43,40 @@ function clearGrid() {
     let etchCellLength = etchCell.length
     for (let x = 0; x < etchCellLength; x++) {
         etchCell[x].style.backgroundColor = "grey";
+    }
+}
+
+function blackBtn() {
+    globalColor = "black";
+    console.log(`blackBtn globalColor is now set to: ${globalColor}`)
+}
+
+function greyBtn() {
+    let rows = document.getElementById("countsize").value;
+    let etchCell = document.getElementsByClassName("etchcell");
+    globalColor = "black"
+    console.log(`greyBtn globalColor is now set to: ${globalColor}`)
+    for (let i = 0; i < rows * rows; i++) {
+        etchCell[i].addEventListener('mouseover', function greyFunc() {
+            if (!etchCell[i].style.opacity){
+                etchCell[i].style.opacity = "0.1";
+            }
+            let cellOpacity = parseFloat(etchCell[i].style.opacity);
+            etchCell[i].style.opacity = cellOpacity + 0.1;
+            console.log(`etchCell[i].style.opacity is ${etchCell[i].style.opacity}`)
+            console.log(`cellOpacity is ${cellOpacity}`)
+        })
+    }
+}
+
+function rainbowBtn() {
+    let rows = document.getElementById("countsize").value;
+    let etchCell = document.getElementsByClassName("etchcell");
+    globalColor = "rainbow"
+    for (let i = 0; i < rows * rows; i++) {
+        etchCell[i].addEventListener('mouseover', function rainbowFunc() {
+            etchCell[i].style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        })
     }
 }
 
